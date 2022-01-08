d8b4f87912cebe8b1612b10776054b45fdaac29f
Chrisbuttowski <97352554+Chrisbuttowski@users.noreply.github.com>
2022-01-08 21:59:16 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+// DOCS: https://www.php.net/manual/en/function.stream-context-create.php
+
+
+// Open the file using the HTTP headers set above
+// DOCS: https://www.php.net/manual/en/function.file-get-contents.php
+/*$opts = [
+    "http" => [
+        "method" => "GET",
+        "header" => "Content-Type: text/html,application/json\r\n" .
+    ]
+];
+$context = stream_context_create($opts);
+*/
+Header('Content-Type: application/json','Accept: application/json');
+$api_url='https://api.tracker.gg/api/v2/valorant/standard/matches/riot/7AM%20FakeAnanas%23TTV?type=competitive';
+
+$json_data = file_get_contents($api_url);
+$json_response = json_decode($json_data);
+
+echo json_encode($json_response);
+
+?>
+
+
